893929f05b18ba94673ef09108316a2129c317c4
NeganovD <85576644+NeganovD@users.noreply.github.com>
2021-10-21 21:43:15 +0400

    Update Application.java

--- src/main/java/ru/vsegdada/Application.java
+++ src/main/java/ru/vsegdada/Application.java
@@ -4,8 +4,6 @@ public class Application {
 
     public static void main(String[] args) {
         House house = new House();
-        Owner owner = new Owner();
-        owner.printOwnerName();
         house.printInfoHouse();
 
     }
