7ab44f10e835a94f5ca52a165eb790ea2b91bb8a
NeganovD <85576644+NeganovD@users.noreply.github.com>
2021-10-21 21:43:46 +0400

    Update House.java

--- src/main/java/ru/vsegdada/House.java
+++ src/main/java/ru/vsegdada/House.java
@@ -4,9 +4,10 @@ public class House {
     private String address = "Izhevsk";
     private String color = "White";
     private int floor = 2;
+    Owner Owner = new Owner();
+
 
     public void printInfoHouse() {
-        System.out.println("Address: " + address + ", How much floors: " + floor + ", House's color: " + color);
+        System.out.println("Address: " + address + ", How much floors: " + floor + ", House's color: " + color + ", My Owner: " + Owner);
     }
-
 }
