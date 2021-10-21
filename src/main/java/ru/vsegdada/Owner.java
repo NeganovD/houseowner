e1fab291ab36cd57346a7ea8f1f024fe76c6c8f0
NeganovD <85576644+NeganovD@users.noreply.github.com>
2021-10-21 21:44:07 +0400

    Update Owner.java

--- src/main/java/ru/vsegdada/Owner.java
+++ src/main/java/ru/vsegdada/Owner.java
@@ -1,16 +1,14 @@
 package ru.vsegdada;
 
 public class Owner {
-    private String firstname = "Dmitriy";
-    private String lastname = "Neganov";
+    private String firstname = "Ivan";
+    private String lastname = "Ivanov";
     private int age = 30;
-    private String allInfoOwner = firstname + " " + lastname + ", " + age + " years";
 
-    public void printOwnerName() {
-        System.out.println("Hello, My name is " + firstname + " " + lastname + ", " + age + " years");
-    }
-
-    public void printInfoOwner() {
-        System.out.println(allInfoOwner);
+    @Override
+    public String toString() {
+        return firstname + " "+
+                 lastname + " " + age + " old";
     }
 }
+
